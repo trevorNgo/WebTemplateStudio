74d9fb27e0d73d3066a09c9e4f8e09adad1e9fe2
Kai Bailey <t-kabail@microsoft.com>
2019-04-14 13:40:06 -0700

    Created List page for Vue Mocks

---
+++ mocks/Vue/src/components/ListForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="input-group my-3">
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+      type="text"
+      class="form-control"
+      placeholder="Add text here..."
+      aria-label="Add text here..."
+    >
+    <button class="btn btn-primary ml-2" v-on:click="$emit('onAddListItem')">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListForm",
+  props: {
+    value: String
+  }
+};
+</script>
